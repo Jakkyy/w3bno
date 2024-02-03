eb27bb3f22cdd674d431ede0807f0d37f699ef5f
XII <jacopo.cartabmb@gmail.com>
2024-02-03 15:32:38 +0100

    Initial commit + created tech cards

---
+++ src/app/components/StackCard/StackCard.js
@@ -0,0 +1,42 @@
+import Link from "next/link";
+import data from "./StackCard.json";
+import Image from "next/image";
+
+export default function StackCard() {
+	const carouselItems = ["Item 1", "Item 2", "Item 3", "Item 4"];
+	return (
+		<div className="my-10">
+			<h1 className="font-bold text-4xl text-center mb-8 text-primary">
+				Le nostre tecnologie
+			</h1>
+			<div className="w-full justify-center flex flex-nowrap overflow-hidden">
+				{data.map((element, i) => {
+					return (
+						<div
+							key={i}
+							className="border border-accent mb-10 rounded-2xl py-8 px-6 card items-center flex flex-col h-[415px] w-[340px] bg-tertiary mx-4">
+							<Image
+								src={`/assets/${element.image}`}
+								width={56}
+								alt={`${element.name}'s logo`}
+								height={56}
+								style={{ height: 56 }}
+							/>
+							<h2 className="text-accent my-4 font-bold text-3xl">
+								{element.name}
+							</h2>
+							<h3 className="text-accent text-center text-2xl my-4 leading-7  font-extralight items-center ">
+								{element.description}
+							</h3>
+							<Link
+								className="text-tertiary font-semibold text-2xl my-4 rounded-md py-2 px-4 bg-primary p hover:bg-[#FFCC11] transition duration-300"
+								href="/">
+								SCOPRI DI PIU
+							</Link>
+						</div>
+					);
+				})}
+			</div>
+		</div>
+	);
+}
